000a8a6c216abb74e6b6cd0ae4e99a59af5f75ed
unknown <t.bakeer4@gmail.com>
2018-09-14 23:40:26 -0700

    Get articles from NewYorkTimes and the ability to save and delete them from database

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./components/pages/Home"
+import Header from "./components/Header"
+ import Saved from "./components/pages/Saved"
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { url } from "inspector";
 
 class App extends Component {
   render() {
+    
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    
+    <Router>
+      <div className="container-fluid " >
+        <Header />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/saved" component={Saved}/>
       </div>
+    </Router>
     );
   }
 }
--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
+const mongoose = require("mongoose");
+//const routes = require("./routes");
 const app = express();
-
-// Serve up static assets (usually on heroku)
+const PORT = process.env.PORT || 3001;
+const apiRoutes=require("./routes/apiRoutes")
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+// Add routes, both API and view
+//app.use(routes);
+app.use(apiRoutes)
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/nytreact"
+);
 
-// Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+
+// Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎 ==> Server now on port ${PORT}!`);
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
